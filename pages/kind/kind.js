91870cb8db8ec0793d47fcb84ac8e25bf525b9b4
Raydom <raydom007@163.com>
2017-10-13 18:16:46 +0800

    分类页面

--- pages/kind/kind.js
+++ pages/kind/kind.js
@@ -8,65 +8,57 @@ Page({
    * 页面的初始数据
    */
   data: {
-  
+
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    let that = this;
-    wx.request({
-      url: server +"/admin/album/categorys",
-      success:function (res) {
-        that.setData({
-          kind:res.data
-        })
-        console.log(that.data.kind)
-      }
-    })
+    this.getDate()
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady: function () {
-  
+
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
-  
+
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
   onHide: function () {
-  
+
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
   onUnload: function () {
-  
+
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
   onPullDownRefresh: function () {
-    wx.stopPullDownRefresh()
+    this.getDate();
+
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
   onReachBottom: function () {
-  
+
   },
 
   /**
@@ -79,19 +71,23 @@ Page({
     }
   },
   /**
-   * 事件绑定
+   * 获取数据
    */
-  changeBright:function(){
-    wx.setScreenBrightness({
-      value:0.1,
-      success:function(res){
-        console.log(res)
+  getDate: function () {
+    let that = this;
+    wx.request({
+      url: server + "/kinds",
+      success: function (res) {
+        that.setData({
+          kind: res.data
+        })
+        // 获取数据后暂停下拉刷新
+        wx.stopPullDownRefresh();
+      },
+      error: function () {
+        
       }
     })
-  },
-  addBright: function () {
-    wx.pageScrollTo({
-      scrollTop: 100
-    })
   }
+
 })
